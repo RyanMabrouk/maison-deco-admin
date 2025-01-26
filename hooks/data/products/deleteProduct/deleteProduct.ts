cf3161c0b11f22b7fae81c2361efa9d44615fce9
Rayen Mabrouk <mabroukr999@gmail.com>
2025-01-26 19:59:31 +0100

    Change deleteProduct function to use 'slug' instead of 'id' for deletion

--- hooks/data/products/deleteProduct/deleteProduct.ts
+++ hooks/data/products/deleteProduct/deleteProduct.ts
@@ -4,5 +4,5 @@ import { createClient } from '@/lib/supabase';
 export async function deleteProduct(id: string) {
   const supabase = createClient();
 
-  return await supabase.from('products').delete().eq('id', id);
+  return await supabase.from('products').delete().eq('slug', id);
 }
